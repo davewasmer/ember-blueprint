dc6c0994bb5d78f24bee2afbf6682f86ae7b4411
Dave Wasmer <davewasmer@gmail.com>
2015-11-17 17:29:24 -0700

    update to cli 1.13.12, ember and ED 2.2.0, and add basic style structure

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 /*jshint node:true*/
 
 var stringUtil = require('ember-cli/lib/utilities/string');
+var pkg = require('./package');
 
 module.exports = {
-  description: 'The default blueprint for ember-cli projects.',
+  description: pkg.description,
 
   locals: function(options) {
     var entity    = options.entity;
